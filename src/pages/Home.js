478b72b2ccf21dec6e7a12ed3fd4f975cf450bd9
Thangabalan <thangabalan18@gmail.com>
2025-08-22 10:37:11 +0530

    third

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,32 @@
+// import React from 'react';
+// import './Home.css';
+
+// function Home() {
+//   return (
+//     <div style={{  backgroundColor:'yellow',padding: '50px', textAlign: 'center' }}>
+//       <h1 className="heading">Welcome to Hair Export Company</h1>
+//       <p>We export 100% human hair to countries worldwide.</p>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
 import React from 'react';
 import './Home.css';
 
 function Home() {
   return (
-    <div style={{  backgroundColor:'yellow',padding: '50px', textAlign: 'center' }}>
+    <div style={{ backgroundColor: 'yellow', padding: '50px', textAlign: 'center' }}>
       <h1 className="heading">Welcome to Hair Export Company</h1>
-      <p>We export 100% human hair to countries worldwide.</p>
+      {/* 👇 Apply the animation only to the <p> */}
+      <p className="typewriter">
+        We export 100% human hair to countries worldwide.
+      </p>
     </div>
   );
 }
 
 export default Home;
+
